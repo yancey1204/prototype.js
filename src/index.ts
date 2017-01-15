8caa923c6e69aaa072f79c7a20b13a38d3c4015b
jimzhan <jim.zhan@me.com>
2017-01-15 16:51:16 +0800

    Cycle.js basic sample

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import xs from 'xstream';
 import { run } from '@cycle/xstream-run';
-import { makeDOMDriver } from '@cycle/dom';
+import { makeDOMDriver, h1 } from '@cycle/dom';
 
-console.log('hello typescript');
+
+function main() {
+  const sinks = {
+    DOM: xs.periodic(1000).map(index => h1(`${index} seconds elapsed`))
+  };
+  return sinks;
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers);
