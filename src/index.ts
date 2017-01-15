ff53a4f87a986504f7ea2b2c0d94e95373389ade
jimzhan <jim.zhan@me.com>
2017-01-15 16:28:55 +0800

    typscript/typings supports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('hello prototype.js');
+import xs from 'xstream';
+import { run } from '@cycle/xstream-run';
+import { makeDOMDriver } from '@cycle/dom';
+
+console.log('hello typescript');
